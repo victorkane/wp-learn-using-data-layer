0b3fc379947ad7423c34530578957b1258ecba64
victorkane <victorkane@gmail.com>
2023-02-18 17:44:16 -0300

    feat(Create WP data records): Complete lesson functionaly create WP data records

--- wp-content/plugins/my-first-gutenberg-app/src/CreatePageButton.js
+++ wp-content/plugins/my-first-gutenberg-app/src/CreatePageButton.js
@@ -1,5 +1,9 @@
+import { useSelect } from "@wordpress/data"
+import { store as coreDataStore } from "@wordpress/core-data"
 import { Button, Modal } from "@wordpress/components"
 import { useState } from "@wordpress/element"
+import { useDispatch } from "@wordpress/data"
+import PageForm from "./PageForm"
 
 function CreatePageButton() {
   const [isOpen, setOpen] = useState(false)
@@ -19,9 +23,40 @@ function CreatePageButton() {
   )
 }
 
-function CreatePageForm() {
-  // Empty for now
-  return <div />
+function CreatePageForm({ onCancel, onSaveFinished }) {
+  const [title, setTitle] = useState()
+  const { lastError, isSaving } = useSelect(
+    (select) => ({
+      lastError: select(coreDataStore).getLastEntitySaveError(
+        "postType",
+        "page"
+      ),
+      isSaving: select(coreDataStore).isSavingEntityRecord("postType", "page"),
+    }),
+    []
+  )
+
+  const { saveEntityRecord } = useDispatch(coreDataStore)
+  const handleSave = async () => {
+    const savedRecord = await saveEntityRecord("postType", "page", {
+      title,
+      status: "publish",
+    })
+    if (savedRecord) {
+      onSaveFinished()
+    }
+  }
+  return (
+    <PageForm
+      title={title}
+      onChangeTitle={setTitle}
+      hasEdits={!!title}
+      lastError={lastError}
+      isSaving={isSaving}
+      onCancel={onCancel}
+      onSave={handleSave}
+    />
+  )
 }
 
 export default CreatePageButton
