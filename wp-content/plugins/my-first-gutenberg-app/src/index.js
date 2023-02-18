b8772294fd2c97f38755f5737054475088db4fd4
victorkane <victorkane@gmail.com>
2023-02-18 04:40:36 -0300

    feat(Edit WP data records): Display an Edit form

--- wp-content/plugins/my-first-gutenberg-app/src/index.js
+++ wp-content/plugins/my-first-gutenberg-app/src/index.js
@@ -1,10 +1,36 @@
-import { SearchControl, Spinner, Button } from "@wordpress/components"
+import {
+  SearchControl,
+  Spinner,
+  Button,
+  Modal,
+  TextControl,
+} from "@wordpress/components"
 import { useState, render } from "@wordpress/element"
 import { useSelect } from "@wordpress/data"
 import { store as coreDataStore } from "@wordpress/core-data"
 import { decodeEntities } from "@wordpress/html-entities"
 
-const PageEditButton = () => <Button variant="primary">Edit</Button>
+function PageEditButton({ pageId }) {
+  const [isOpen, setOpen] = useState(false)
+  const openModal = () => setOpen(true)
+  const closeModal = () => setOpen(false)
+  return (
+    <>
+      <Button onClick={openModal} variant="primary">
+        Edit
+      </Button>
+      {isOpen && (
+        <Modal onRequestClose={closeModal} title="Edit page">
+          <EditPageForm
+            pageId={pageId}
+            onCancel={closeModal}
+            onSaveFinished={closeModal}
+          />
+        </Modal>
+      )}
+    </>
+  )
+}
 
 function MyFirstApp() {
   const [searchTerm, setSearchTerm] = useState("")
@@ -62,13 +88,19 @@ function PagesTable({ hasResolved, pages }) {
   )
 }
 
-function PagesList({ pages }) {
+export function EditPageForm({ pageId, onCancel, onSaveFinished }) {
   return (
-    <ul>
-      {pages?.map((page) => (
-        <li key={page.id}>{decodeEntities(page.title.rendered)}</li>
-      ))}
-    </ul>
+    <div className="my-gutenberg-form">
+      <TextControl value="" label="Page title:" />
+      <div className="form-buttons">
+        <Button onClick={onSaveFinished} variant="primary">
+          Save
+        </Button>
+        <Button onClick={onCancel} variant="tertiary">
+          Cancel
+        </Button>
+      </div>
+    </div>
   )
 }
 
