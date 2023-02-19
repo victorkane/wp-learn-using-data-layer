ae8e1be33230f3cf217606b7e59c40856e95c8e6
victorkane <victorkane@gmail.com>
2023-02-19 10:14:23 -0300

    feat(Delete WP data records): Add visual feedback (spinner)

--- wp-content/plugins/my-first-gutenberg-app/src/DeletePageButton.js
+++ wp-content/plugins/my-first-gutenberg-app/src/DeletePageButton.js
@@ -1,13 +1,31 @@
-import { Button } from "@wordpress/components"
+import { Button, Spinner } from "@wordpress/components"
 import { store as coreDataStore } from "@wordpress/core-data"
 import { useDispatch } from "@wordpress/data"
+import { useSelect } from "@wordpress/data"
 
 const DeletePageButton = ({ pageId }) => {
   const { deleteEntityRecord } = useDispatch(coreDataStore)
   const handleDelete = () => deleteEntityRecord("postType", "page", pageId)
+  const { isDeleting } = useSelect(
+    (select) => ({
+      isDeleting: select(coreDataStore).isDeletingEntityRecord(
+        "postType",
+        "page",
+        pageId
+      ),
+    }),
+    [pageId]
+  )
   return (
-    <Button variant="primary" onClick={handleDelete}>
-      Delete
+    <Button variant="primary" onClick={handleDelete} disabled={isDeleting}>
+      {isDeleting ? (
+        <>
+          <Spinner />
+          Deleting...
+        </>
+      ) : (
+        "Delete"
+      )}
     </Button>
   )
 }
