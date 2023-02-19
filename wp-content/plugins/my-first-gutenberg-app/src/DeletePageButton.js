c2f6f70c020e86ea1ad08ef608e1122235f8e007
victorkane <victorkane@gmail.com>
2023-02-19 10:02:14 -0300

    feat(Delete WP data records): implement delete functionality for button (gratis re-render of page list)

--- wp-content/plugins/my-first-gutenberg-app/src/DeletePageButton.js
+++ wp-content/plugins/my-first-gutenberg-app/src/DeletePageButton.js
@@ -1,5 +1,15 @@
 import { Button } from "@wordpress/components"
+import { store as coreDataStore } from "@wordpress/core-data"
+import { useDispatch } from "@wordpress/data"
 
-const DeletePageButton = () => <Button variant="primary">Delete</Button>
+const DeletePageButton = ({ pageId }) => {
+  const { deleteEntityRecord } = useDispatch(coreDataStore)
+  const handleDelete = () => deleteEntityRecord("postType", "page", pageId)
+  return (
+    <Button variant="primary" onClick={handleDelete}>
+      Delete
+    </Button>
+  )
+}
 
 export default DeletePageButton
